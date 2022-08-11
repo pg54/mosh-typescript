7447436e6575692deb8bf2e6c3f63ae72858acb4
pg54 <1260424774@qq.com>
2022-08-11 09:23:11 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,24 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+class Store<T> {
+  private _objects: T[] = [];
+  add(obj: T): void {
+    this._objects.push(obj)
+  }
+}
+
+// 拓展geneirc class
+class CompressibleStore<T> extends Store<T> {
+  compress() {}
+
+}
+
+let cp = new CompressibleStore<Product>();
+cp.add({name: 'apple', price: 10})
